cc3e197d2b39bea6ce29a20512c55b0fdb241d1e
wang <43001458+Mrwangluhua@users.noreply.github.com>
2018-11-13 13:30:27 +0800

    Unit04-Test4-1日历
    打印今天所在月份的日历，当前日用*号标记

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        LocalDate date = LocalDate.now();
+        int mouth = date.getMonthValue();
+        int year = date.getYear();
+        int day = date.getDayOfMonth();
+        System.out.println(mouth+"\t"+day+"%3d"+year);
+    }
+}
